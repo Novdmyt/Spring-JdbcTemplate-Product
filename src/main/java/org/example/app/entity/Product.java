c63a231bc5506062bcd33efefcdb2d576435352b
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-12 20:02:54 +0300

    Add constructor and getters setters

---
+++ src/main/java/org/example/app/entity/Product.java
@@ -0,0 +1,83 @@
+package org.example.app.entity;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Component
+public class Product {
+    private Integer id;
+    private String name;
+    private String quota;
+    private double price;
+
+    public Product(String name, String quota, double price) {
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    public Product(Integer id, String name, String quota, double price) {
+        this.id = id;
+        this.name = name;
+        this.quota = quota;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        if (Double.compare(price, product.price) != 0) return false;
+        if (!Objects.equals(id, product.id)) return false;
+        if (!Objects.equals(name, product.name)) return false;
+        return Objects.equals(quota, product.quota);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (quota != null ? quota.hashCode() : 0);
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuota() {
+        return quota;
+    }
+
+    public void setQuota(String quota) {
+        this.quota = quota;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
