332b4eacedd224ca8191c7e044031e3dd21ad6b5
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-12 20:12:09 +0300

    Add constructor getData

---
+++ src/main/java/org/example/app/view/ProductCreateView.java
@@ -0,0 +1,26 @@
+package org.example.app.view;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Scanner;
+
+@Component
+public class ProductCreateView {
+    public String[] getData() {
+        Scanner scanner = new Scanner(System.in);
+        String title = "Input  name: ";
+        System.out.print(title);
+        String firstName = scanner.nextLine().trim();
+        title = "Input quota: ";
+        System.out.print(title);
+        String lastName = scanner.nextLine().trim();
+        title = "price: ";
+        System.out.print(title);
+        String phone = scanner.nextLine().trim();
+        return new String[]{firstName, lastName, phone};
+    }
+
+    public void getOutput(String output) {
+        System.out.println(output);
+    }
+}
