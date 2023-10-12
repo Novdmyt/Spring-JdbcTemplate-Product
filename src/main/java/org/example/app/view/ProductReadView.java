1145c549cc575f2b6c71d445478ab9b06e4c1af2
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-12 23:51:40 +0300

    Add @Component

--- src/main/java/org/example/app/view/ProductReadView.java
+++ src/main/java/org/example/app/view/ProductReadView.java
@@ -1,5 +1,8 @@
 package org.example.app.view;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductReadView {
     public void getOutput(String output) {
         System.out.println(output);
